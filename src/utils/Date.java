7655a0b5b5500dfa928b07347337265a2138ebab
Mathis Beauville <mathis.beauville@gmail.com>
2022-05-16 16:29:09 +0200

    Remove debug code

--- src/utils/Date.java
+++ src/utils/Date.java
@@ -4,10 +4,4 @@ public class Date {
     public static boolean validateDate(String date) {
         return date.matches("^\\d{2}/\\d{2}/\\d{4}$");
     }
-
-    // DEBUG PURPOSES ONLY
-    // public static void main(String[] args) {
-    // String test = "12/12/1000";
-    // System.out.println(test.matches("^\\d{2}/\\d{2}/\\d{4}$"));
-    // }
 }
