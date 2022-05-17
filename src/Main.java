151ce1f3046da81db69a40b5b0eab9e27032e752
Mathis Beauville <mathis.beauville@gmail.com>
2022-05-17 20:08:30 +0200

    potential final

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-import model.Shop;
-import view.*;
+import view.Login;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Login login = new Login();
-        // login.pack();
-        // login.setSize(500, 500);
-        // login.setVisible(true);
-        Shop shop = new Shop("A super computer shop :)", "password",
-                "Paris 9 75009 (Paris), 8 Rue De Londres");
-        new MainInterface(shop);
+        Login login = new Login();
+        login.pack();
+        login.setSize(500, 500);
+        login.setVisible(true);
     }
 }
